ce364f351d281cbddb75838eb49cefb7fe9fadab
jhonatandvid <jhonatandblanco@gmail.com>
2023-03-30 16:48:20 -0400

    hito_2

--- hito_2/EVALUACION HITO 2/H2Defensa/H2Main.java
+++ hito_2/EVALUACION HITO 2/H2Defensa/H2Main.java
@@ -3,51 +3,53 @@ package H2Defensa;
 public class H2Main {
     public static void main(String[] args)
     {
-
-        Provincia pro = new Provincia("Murillo");
-        Provincia pro1 = new Provincia("Aroma");
+        //2) AGREGAR 2 DEPARTAMENTOS CON 2 PROVINCIAS CADA UNO A EL PAIS BOLIVIA
+        //PROVINCIAS
+        Provincia proLa1 = new Provincia("Murillo");
+        Provincia proLa2 = new Provincia("Aroma");
         //////////////
-        Provincia pr = new Provincia("Gran chaco");
-        Provincia pr1 = new Provincia("Cercado");
+        Provincia proTa1 = new Provincia("Gran chaco");
+        Provincia proTa2 = new Provincia("Cercado");
 
-        //provincias
+        Provincia[] provinciasLapaz = new Provincia[2];
+        provinciasLapaz[0] = proLa1;
+        provinciasLapaz[1] = proLa2;
 
-        Provincia[] provincias = new Provincia[2];
-        provincias[0] = pro;
-        provincias[1] = pro1;
-
-        ////////////
-        Provincia[] provincias2 = new Provincia[2];
-        provincias2[0] = pr;
-        provincias2[1] = pr1;
+        Provincia[] provinciasTarija = new Provincia[2];
+        provinciasTarija[0] = proTa1;
+        provinciasTarija[1] = proTa2;
 
         //DEPARTAMENTOS
-        Departamento de = new Departamento("LA PAZ",2,provincias);
-        Departamento de1 = new Departamento("TARIJA",2,provincias2);
+        Departamento deLpz = new Departamento("LA PAZ",2,provinciasLapaz);
+        Departamento deTrj = new Departamento("TARIJA",2,provinciasTarija);
 
 
         Departamento[] deps = new Departamento[2];
-        deps[0] = de;
-        deps[1] = de1;
+        deps[0] = deLpz;
+        deps[1] = deTrj;
 
         Pais pa = new Pais("Bolivia",2,deps);
 
+        System.out.println("2) DATOS DEL PAIS: ");
         pa.MuestraPais();
+        System.out.println(" ");
 
-        Provincia pro3 = new Provincia("Quillacollo");
-        Provincia[] provincia3 = new Provincia[1];
-        provincia3[0] = pro3;
-        Departamento de3 = new Departamento("COCHABAMBA",1,provincia3);
-
-
-        pa.agregaDepartamento(de3);
-
+        //3) AGREGAR UN NUEVO DEPARTAMENTO Y UNA NUEVA PROVINCIA
+        Provincia proCbba1 = new Provincia("Quillacollo");
+        Provincia[] provinciasCbba = new Provincia[1];
+        provinciasCbba[0] = proCbba1;
+        Departamento deCbba = new Departamento("COCHABAMBA",1,provinciasCbba);
+        System.out.println("3) DATOS DEL PAIS ACTUALIZADO DE ACUERDO AL PUNTO 3: ");
+        pa.agregaDepartamento(deCbba);
         pa.MuestraPais();
+        System.out.println("   ");
 
-        System.out.println("los nuevos registros son: ");
-        Provincia pro4 = new Provincia("Ingavi");
-        //AGREGARE UNA PROVINCIA A EL DEPARTAMENTO LA PAZ
-        de.agregaNuevaProvincia("LA PAZ",pro4);
+        //4) AGREGAR UNA NUEVA PROVINCIA EN UN DEPARTAMENTO YA CREADO
+        System.out.println("4) LA NUEVA PROVINCIA ES:");
+        Provincia proLa3 = new Provincia("Ingavi");
+
+        //AGREGARE UNA PROVINCIA A EL DEPARTAMENTO LA PAZ por eso se usa el "deLpz"
+        deLpz.agregaNuevaProvincia("LA PAZ",proLa3);
         pa.MuestraPais();
 
 
